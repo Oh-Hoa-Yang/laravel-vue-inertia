f703fedef4a31318bc72ecb3ca8e73a9dcd6913d
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2026-02-04 22:28:13 +0800

    make it same as current tutorial, actually we can just edit the Welcome.vue and no changes at here

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+// use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome');
-})->name('home');
+    return inertia('Index/index');
+});
+//     return Inertia::render('Index/index');
+// })->name('home');
