09ebee78ab45d70bc1a83f0593189c7580dbc031
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2026-02-27 22:48:31 +0800

    feat: added progress 25 - Passing data to pages ~> using props(vue).

--- resources/js/pages/Index/index.vue
+++ resources/js/pages/Index/index.vue
@@ -14,8 +14,15 @@ setInterval(() => {
 <template>
     <div>Index</div>
     <Link href="/hello">Show Page</Link>
+    <div>
+        The message is {{ message }}
+    </div>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+
+defineProps({
+    message: String
+})
 </script>
