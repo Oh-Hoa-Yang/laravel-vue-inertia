193cd366bbe85b43863772e46dfce00bd5376d45
Oh-Hoa-Yang <ohhaoyang1@gmail.com>
2026-03-01 00:28:23 +0800

    feat: implement MainLayout component for consistent page structure in Index and Show pages -Progress 25

--- resources/js/pages/Index/index.vue
+++ resources/js/pages/Index/index.vue
@@ -12,15 +12,18 @@ setInterval(() => {
 </script> -->
 
 <template>
-    <div>Index</div>
-    <Link href="/hello">Show Page</Link>
-    <div>
-        The message is {{ message }}
-    </div>
+    <MainLayout>
+        <div>Index</div>
+        <Link href="/hello">Show Page</Link>
+        <div>
+            The message is {{ message }}
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import MainLayout from '@/Layouts/MainLayout.vue';
 
 defineProps({
     message: String
