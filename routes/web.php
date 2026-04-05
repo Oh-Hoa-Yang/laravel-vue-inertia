2624ce5de3f7a066846f19f147ae0ae249bf2bae
Oh Hoa Yang <thunderohy@Ohs-MacBook-Air.local>
2026-04-05 22:19:08 +0800

    feat: add p43 V-for, v-bind, components for reusable

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\IndexController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 // use Inertia\Inertia;
 
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/',[<class name - controller>::class, '<method name>'])
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello',[IndexController::class, 'show']);
+
+Route::resource('/listing', ListingController::class)->only('index', 'show');
