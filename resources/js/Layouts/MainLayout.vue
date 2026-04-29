b78a9bb18f8ef2e62ab3d68009a441c3e1545cd1
Oh Hoa Yang <thunderohy@Ohs-MacBook-Air.local>
2026-04-29 16:59:11 +0800

    feat: add progress till 47 - mostly about flash message

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,15 +1,39 @@
 <template>
     <Link href="/">Main Page</Link>&nbsp;
     <Link href="/hello">Show Page</Link>
-    <div>The page with time {{ timer }}</div>
+    <!-- <div>The page with time {{ timer }}</div> -->
+     <!-- Display the flash message -->
+      <div v-if="flashSuccess" class="success">
+        {{ flashSuccess }}
+      </div>
+      <div>
+        {{ y }}
+      </div>
     <slot></slot>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/vue3';
-import { ref } from 'vue';
+import { Link, usePage } from '@inertiajs/vue3';
+import { ref, computed } from 'vue';
 
-const timer = ref(0);
+const x = ref(0)
+const y = computed(() => x.value * 2)
+// import { ref } from 'vue';
 
-setInterval(() => timer.value++, 1000);
+// const timer = ref(0);
+
+// setInterval(() => timer.value++, 1000);
+
+// To access the data shared through this middleware, you will use page.props.value as this is a ref and then the structure of your data (in HandleInertiaRequests) is flash.success
+// page.props.value.flash.success
+const page = usePage()
+
+const flashSuccess = computed(() => page.props.flash?.success )
 </script>
+
+<style scoped>
+.success{
+    background-color: green;
+    color: white;
+}
+</style>
