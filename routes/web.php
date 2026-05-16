501bf4194103c42737c48bcaa15e2a3a891060b2
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-16 14:16:55 +0800

    add create form for register - P79 User Registration Form

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 use Illuminate\Support\Facades\Route;
 // use Inertia\Inertia;
 
@@ -27,11 +28,15 @@ Route::resource('listing', ListingController::class)
 Route::resource('listing', ListingController::class)
     ->except(['create', 'store', 'edit', 'update', 'destroy']);
 
-Route::get('login',[AuthController::class, 'create'])->name('login');
-Route::post('login',[AuthController::class, 'store'])->name('login.store');
-Route::delete('logout',[AuthController::class, 'destroy'])->name('logout');
+Route::get('login',[AuthController::class, 'create'])
+    ->name('login');
+Route::post('login',[AuthController::class, 'store'])
+    ->name('login.store');
+Route::delete('logout',[AuthController::class, 'destroy'])
+    ->name('logout');
 
-
+Route::resource('user-account', UserAccountController::class)
+    ->only(['create']);
 
 
 // Only allow the show of 'index' and 'show'; others -> need to check middleware auth (Better Understanding Version)
