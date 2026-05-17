a50e8e83537e62d767b686e2270c1b83bb2599fe
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-17 23:55:42 +0800

    feat: add Password Hashing -> removed from Controller and moved to User.php as a Laravel Automatically Hash Password - P82 Eloquent Mutators & Accessors ~Hashing Password Example

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -14,12 +14,19 @@ class UserAccountController extends Controller
 
     public function store(Request $request) {
         // We use make() method -> doesn't store a model immediately in the table, so we need to have $user->save(); ELSE; just use create() method without the $user->save();
-        $user = User::make($request->validate([ 
+        $user = User::create($request->validate([ 
             'name' => 'required',
             'email' => 'required|email|unique:users',
             'password' => 'required|min:8|confirmed' // If you use 'confirmed' validator, you've got one field called 'password', but then a lot of us would expect another data filled send called password_confirmation
         ]));
-        $user->save();
+        // $user->password = Hash::make($user->password); 
+        // Now, we don't have to call Hash::make() as we already do it in the model (User.php) 
+        // $user->password = $user->passsword //since you see both are the same -> 1 = 1 ; which is the same, this means that probably we don't need this at all. 
+        // Also means that the password attribute can now be just safely set automatically using mass assignment.
+        // As a reminder, mass assignment is when you use make or create methods to set the column values.
+
+        // Since we don't do any extra thing over here, so, we dont need this here, and so, we also don't need to use make method, instead we use create method
+        // $user->save();
 
         Auth::login($user);
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 
 class User extends Authenticatable
 {
@@ -45,4 +47,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    // last time Laravel 9.0x, we need to hash the password manually, if we did this here, we do not need to do // $user->password = Hash::make($user->password); in UserAccountController.php 
+    // camelCase -> kebab_case
+    protected function password(): Attribute
+	 {
+		 return Attribute::make(
+			 get: fn ($value) => $value,
+			 set: fn ($value) => Hash::make($value),
+		 );
+	 }
 }
