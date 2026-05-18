f02bad3a1d620c6c9532dfaa00ee64961ac2369e
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-18 16:40:46 +0800

    feat: add the dd() for testing the concept of how hashing of the passwords work - P83 How Hashing of The Passwords Work

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 // use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class IndexController extends Controller
 {
@@ -12,6 +13,12 @@ class IndexController extends Controller
         // dd(Listing::all());
         // dd(Auth::user()); //can use to check all about the authenticated user
         // dd(Auth::check()); // use to check whether the user is authenticated user or not
+
+        dd(
+            Hash::make('password'),
+            '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi',
+            Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi')
+        );
 
         return inertia(
             'Index/Index',
