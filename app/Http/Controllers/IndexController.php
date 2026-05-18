0a1759a331819666f3814f902c5ae28ec3b76a7e
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-18 16:42:05 +0800

    refactor: comment the test of how hashing of passwords work - P83 How Hashing of The Passwords Work

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 // use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+// use Illuminate\Support\Facades\Hash;
 
 class IndexController extends Controller
 {
@@ -14,11 +14,11 @@ class IndexController extends Controller
         // dd(Auth::user()); //can use to check all about the authenticated user
         // dd(Auth::check()); // use to check whether the user is authenticated user or not
 
-        dd(
-            Hash::make('password'),
-            '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi',
-            Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi')
-        );
+        // dd(
+        //     Hash::make('password'),
+        //     '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi',
+        //     Hash::check('password', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro911C/.og/at2.uheWG/igi')
+        // );
 
         return inertia(
             'Index/Index',
