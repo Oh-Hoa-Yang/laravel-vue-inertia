0d06ad5212fafc95b07f17d86662487a446a6ff1
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-23 00:00:18 +0800

    feat: add P92

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,11 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 use function Laravel\Prompts\form;
 
 class ListingController extends Controller
 {
+    // Since this ListingController is a resource controller, this means we can use the third way, the simplest way
+    // THIS IS NO LONGER BE USE, IF U USE RESOURCE CONTROLLER, YOU NEED TO DEFINE IT IN web.php 
+    // public function __construct()
+    // {
+    //     $this->authorizeResource(Listing::class, 'listing');
+    // }
+
+
     /**
      * Display a listing of the resource.
      */
@@ -27,6 +37,7 @@ class ListingController extends Controller
      */
     public function create()
     {
+        Gate::authorize('create', Listing::class); // Remember this need to have class, so Laravel can figure out which model policy do you mean
         return inertia('Listing/Create');
     }
 
@@ -99,6 +110,20 @@ class ListingController extends Controller
     // Laravel will instead fetch the model by the given ID from the route parameter and it will be immediately available in this show() method, so you don't have to call find() anymore.
     public function show(Listing $listing)
     {
+        // you can either try to get the current user and check for this user permissions, or you can use the controller helper method called authorize
+        // 'view' is the function name from ListingPolicy.php
+
+        // FIRST example
+        // if (Auth::user()->cannot('view', $listing)){
+        //     abort(403); // this is our HTTP code for the (action forbidden 禁止行为)
+        // }; 
+        //Another example is the authorize method from the controller
+        //this authorize would do what you have here in the 3 commented lines above -> if (Auth::user()->cannot('view', $listing)){};
+        // it would check if the current user is authorized to perform this view operation on this model (Listing in here), if not, it would automatically return 403 code from this controller action (referring to the authorize() method). 
+        // $this->authorize('view', $listing); //This is the outdated way of doing
+        Gate::authorize('view', $listing); // The modern Laravel 12 replacement for $this->authorize()
+
+
         return inertia(
             'Listing/Show',
             [
