675cb723b515a680ea5d516a9696903b67c448b1
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-26 23:35:12 +0800

    feat: add pagination - P98 Adding Pagination

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-        <Listing v-for="listing in listings" :key="listing.id" :listing="listing" />
+        <!-- listing would be found under listings.data -->
+        <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
+    </div>
+
+    <!-- conditional rendering - v-if -->
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-8">
+        <!-- .links is the property on this new object (in defineProps) that Laravel returns when you call this paginate (->paginate(10) in ListingController) -->
+        <Pagination :links="listings.links" />
     </div>
 </template>
 
 <script setup>
+import Pagination from '@/Components/UI/Pagination.vue';
 import Listing from './Index/Components/Listing.vue';
 
 defineProps({
-    listings: Array,
+    // Become an Object
+    listings: Object,
 })
 </script>
