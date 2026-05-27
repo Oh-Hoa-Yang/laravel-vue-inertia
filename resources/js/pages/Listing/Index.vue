7d60172b990d1468730a61fa8af0a0b9bf575c0c
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-27 14:48:56 +0800

    feat: add a form for filter - P99 Filtering Form Markup

--- resources/js/pages/Listing/Index.vue
+++ resources/js/pages/Listing/Index.vue
@@ -1,11 +1,12 @@
 <template>
+    <Filters />
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <!-- listing would be found under listings.data -->
         <Listing v-for="listing in listings.data" :key="listing.id" :listing="listing" />
     </div>
 
     <!-- conditional rendering - v-if -->
-    <div v-if="listings.data.length" class="w-full flex justify-center mt-8 mb-8">
+    <div v-if="listings.data.length" class="w-full flex justify-center mt-4 mb-4">
         <!-- .links is the property on this new object (in defineProps) that Laravel returns when you call this paginate (->paginate(10) in ListingController) -->
         <Pagination :links="listings.links" />
     </div>
@@ -13,6 +14,7 @@
 
 <script setup>
 import Pagination from '@/Components/UI/Pagination.vue';
+import Filters from './Index/Components/Filters.vue';
 import Listing from './Index/Components/Listing.vue';
 
 defineProps({
