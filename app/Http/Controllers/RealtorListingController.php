a78f50adcd2f44c903b2d97d0afc1d71019a14cd
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-05-29 18:26:26 +0800

    feat: add the appearance of Index Listing (Personal) - P107 Showing the Realtor Listings

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RealtorListingController extends Controller
 {
     public function index()
     {
-        return inertia('Realtor/Index');
+        // dd(Auth::user()->listings); // for testing whether it can fetch all the records of that user 
+        return inertia(
+            'Realtor/Index',
+            ['listings' => Auth::user()->listings]
+        );
     }
 }
