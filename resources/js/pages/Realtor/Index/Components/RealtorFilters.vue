55a46526c38fdcd120c5d45ba97e978df043fe3e
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-02 00:04:37 +0800

    feat: add logic to the checkbox - P111 Vue Watchers - Filtering Using Checkboxes

--- resources/js/pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/pages/Realtor/Index/Components/RealtorFilters.vue
@@ -2,9 +2,36 @@
     <form>
         <div class="mb-4 mt-4 flex flex-wrap gap-2">
             <div class="flex flex-nowrap items-center gap-2">
-                <input type="checkbox" id="deleted" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                <input 
+                    v-model="filterForm.deleted"
+                    type="checkbox" 
+                    id="deleted" 
+                    class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
                 <label for="deleted">Deleted</label>
             </div>
         </div>
     </form>
 </template>
+
+<script setup>
+import { reactive, watch } from 'vue';
+import { router } from '@inertiajs/vue3';
+
+const filterForm = reactive({
+    deleted: false,
+})
+
+watch(
+    // In old way, we use Inertia.get() and we need to import {Inertia} from '@inertiajs/inertia'
+    // but now, we use router.get() and we need to import { router } from '@inertiajs/vue3'
+    filterForm, () => router.get(
+        route('realtor.listing.index'),
+        filterForm,
+        {
+            preserveState: true,
+            preserveScroll: true,
+        }
+    )
+)
+</script>
