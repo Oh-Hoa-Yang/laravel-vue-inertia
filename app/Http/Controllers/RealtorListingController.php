794eda340fd8575e20f140409a19f12893c45dac
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-02 21:58:55 +0800

    feat: set the logic of backend part about checkboxes filter - P113 Filtering Using Checkboxes - The Backend Part

--- app/Http/Controllers/RealtorListingController.php
+++ app/Http/Controllers/RealtorListingController.php
@@ -9,13 +9,28 @@ use Illuminate\Support\Facades\Gate;
 
 class RealtorListingController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        // dd($request->all());
+        // dd($request->get('deleted')); //?deleted=true; output: true
+        // dd($request->get('deleted') === true);//?deleted=true; output: false
+        // dd((bool)$request->get('deleted') === true); //?deleted=true; output: true | ?deleted=false; output: true 
+        // it still show 'true', so, to actually convert this value to a Boolean, you would need to call a specific method of the request, which is call boolean
+        // dd((bool)$request->boolean('deleted')); // and now, ?deleted=true or =1 or =yes; output:true | ?deleted=false or =0 or =no; output:false
         Gate::authorize('viewAny', Listing::class);
+        $filters = [
+            'deleted' => $request->boolean('deleted')
+        ];
         // dd(Auth::user()->listings); // for testing whether it can fetch all the records of that user 
         return inertia(
             'Realtor/Index',
-            ['listings' => Auth::user()->listings]
+            [
+                'listings' => Auth::user()
+                    ->listings()
+                    ->mostRecent()
+                    ->filter($filters)
+                    ->get()
+            ]
         );
     }
 
