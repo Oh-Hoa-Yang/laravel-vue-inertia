95359a0e621b5d70204d33e38eb2db359d7d8001
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-05 01:00:58 +0800

    fix: url value deleted=true when refreshed, ticked became unticked, so fixed it to be the same true then refreshed still remain ticked checkbox - P116 Filtering Form - Default Values

--- resources/js/pages/Realtor/Index.vue
+++ resources/js/pages/Realtor/Index.vue
@@ -1,7 +1,7 @@
 <template>
     <h1 class="text-3xl mb-4">Your Listings</h1>
     <section>
-        <RealtorFilters />
+        <RealtorFilters :filters="filters" />
     </section>
     <section class="grid grid-cols-1 lg:grid-cols-2 gap-2">
         <Box v-for="listing in listings" :key="listing.id">
@@ -39,6 +39,7 @@ import RealtorFilters from "./Index/Components/RealtorFilters.vue"
 
 defineProps({
     listings: Array,
+    filters: Object,
 })
 </script>
 
