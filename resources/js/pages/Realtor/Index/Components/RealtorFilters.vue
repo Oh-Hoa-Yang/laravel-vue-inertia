202f7675c0343a5456ae5aaa665dc88d3c1b8747
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-04 23:41:10 +0800

    feat: add P115 - Customizable Sorting - Front End

--- resources/js/pages/Realtor/Index/Components/RealtorFilters.vue
+++ resources/js/pages/Realtor/Index/Components/RealtorFilters.vue
@@ -1,7 +1,7 @@
 <template>
     <form>
         <div class="mb-4 mt-4 flex flex-wrap gap-2">
-            <div class="flex flex-nowrap items-center gap-2">
+            <div class="flex flex-nowrap items-center gap-4">
                 <input 
                     v-model="filterForm.deleted"
                     type="checkbox" 
@@ -10,6 +10,18 @@
                 />
                 <label for="deleted">Deleted</label>
             </div>
+
+            <div>
+                <select class="input-filter-l w-24" v-model="filterForm.by">
+                    <option value="created_at">Added</option>
+                    <option value="price">Price</option>
+                </select>
+                <select class="input-filter-r w-32" v-model="filterForm.order">
+                    <option v-for="option in sortOptions" :key="option.value" :value="option.value">
+                        {{ option.label }}
+                    </option>
+                </select>
+            </div>
         </div>
     </form>
 </template>
@@ -17,10 +29,37 @@
 <script setup>
 import { router } from '@inertiajs/vue3';
 import { debounce } from 'lodash';
-import { reactive, watch } from 'vue';
+import { reactive, watch, computed } from 'vue';
+
+const sortLabels = {
+    created_at: [
+        {
+            label: 'Latest',
+            value: 'desc'
+        },
+        {
+            label: 'Oldest',
+            value: 'asc'
+        }
+    ],
+    price: [
+    {
+            label: 'Pricey',
+            value: 'desc'
+        },
+        {
+            label: 'Cheapest',
+            value: 'asc'
+        }
+    ]
+}
+
+const sortOptions = computed(() => sortLabels[filterForm.by])
 
 const filterForm = reactive({
     deleted: false,
+    by: 'created_at',
+    order: 'desc'
 })
 
 watch(
