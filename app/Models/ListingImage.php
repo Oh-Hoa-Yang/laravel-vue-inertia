c62bcf738bb21f32ad56b3c50891d62c9983b1b7
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-06 00:18:39 +0800

    feat: created ListingImage model and migration, set relationship - P121 File Data Model

---
+++ app/Models/ListingImage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Listing;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ListingImage extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['filename'];
+
+    public function listing(): BelongsTo
+    {
+        return $this->belongsTo(Listing::class);
+        // Notice how I haven't specified the second argument to this belongs to method, which is the column name to use for the foreign key, as Laravel has a mechanism to automatically figure out this column name. 
+        // In this example, if you want specify a custom column name, Laravel will just use the relation name 'listing():BelongsTo' underscore id, which will become 'listing_id'
+    }
+}
