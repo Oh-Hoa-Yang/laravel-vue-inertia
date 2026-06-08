734a5a88a45ad758d6766b3bb589ac21d18ee641
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-08 23:51:46 +0800

    feat: add P124 Uploading Multiple Images (Backend Part) - write store method of controller, make some changes to view page

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\ListingImage;
 use Illuminate\Http\Request;
 
 class RealtorListingImageController extends Controller
@@ -17,6 +18,18 @@ class RealtorListingImageController extends Controller
 
     public function store(Listing $listing, Request $request)
     {
-        dd('Works!');
+        if ($request->hasFile('images')) {
+            // we can submit multiple file 
+            foreach ($request->file('images') as $file) {
+            $path = $file->store('images', 'public'); // store it to images folder in public disk -> refer to filesystemns.php 
+
+
+            // Next step, to store the database model
+            $listing->images()->save(new ListingImage([
+                'filename' => $path
+            ]));
+            }
+        }
+        return redirect()->back()->with('success', 'Images uploaded!');
     }
 }
--- resources/js/pages/Realtor/ListingImage/Create.vue
+++ resources/js/pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,12 @@
 <template>
-    <form method="POST" :action="route('realtor.listing.image.store', {listing: listing.id})">
-        <input type="file" multiple name="files" />
+    <!-- enctype="multipart/form-data" it's just something you need to do every time you upload files -->
+    <form 
+        enctype="multipart/form-data"
+        method="POST" 
+        :action="route('realtor.listing.image.store', {listing: listing.id})">
+        <!-- This 'name' will be using inside the controller, not 'files' (edited) -->
+         <!-- We accept multiple files always, even if you just upload one file for that. The name should contain square bracket like an array of things -->
+        <input type="file" multiple name="images[]" /> 
         <button type="submit">Send</button>
     </form>
 </template>
