dfbef7604fe64213befb49eead7c81ba47ad1305
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-09 22:37:39 +0800

    feat: add P125 Uploading Multiple Images (In Vue)

--- resources/js/pages/Realtor/ListingImage/Create.vue
+++ resources/js/pages/Realtor/ListingImage/Create.vue
@@ -1,18 +1,44 @@
 <template>
-    <!-- enctype="multipart/form-data" it's just something you need to do every time you upload files -->
-    <form 
-        enctype="multipart/form-data"
-        method="POST" 
-        :action="route('realtor.listing.image.store', {listing: listing.id})">
-        <!-- This 'name' will be using inside the controller, not 'files' (edited) -->
-         <!-- We accept multiple files always, even if you just upload one file for that. The name should contain square bracket like an array of things -->
-        <input type="file" multiple name="images[]" /> 
-        <button type="submit">Send</button>
-    </form>
+    <Box>
+        <template #header>
+            Upload New Images
+        </template>
+        <form @submit.prevent="upload">
+            <!-- This 'name' will be using inside the controller, not 'files' (edited) -->
+             <!-- We accept multiple files always, even if you just upload one file for that. The name should contain square bracket like an array of things -->
+            <input type="file" multiple @input="addFiles" /> 
+            <button type="submit" class="btn-outline">Upload</button>
+            <button type="reset" class="btn-outline" @click="reset">Reset</button>
+        </form>
+    </Box>
 </template>
 
 <script setup>
-defineProps({
+import { useForm } from '@inertiajs/vue3';
+import Box from '@/Components/UI/Box.vue';
+
+const props = defineProps({
     listing: Object
 })
+
+const form = useForm({
+    images: [],
+})
+
+const upload = () => {
+    form.post(
+        route('realtor.listing.image.store', { listing: props.listing.id}),
+        {
+            onSuccess: () => form.reset('images'),
+        }
+    )
+}
+
+const addFiles = (event) => {
+    for (const image of event.target.files) {
+        form.images.push(image)
+    }
+}
+
+const reset = () => form.reset('images')
 </script>
