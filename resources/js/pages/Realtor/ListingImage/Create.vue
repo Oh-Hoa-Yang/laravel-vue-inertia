1ae7b304f8f702e197eca8971c379c6e6d9dceb0
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-10 21:22:33 +0800

    feat: add P128 - Displaying the Uploaded Images

--- resources/js/pages/Realtor/ListingImage/Create.vue
+++ resources/js/pages/Realtor/ListingImage/Create.vue
@@ -16,12 +16,19 @@
 </template>
 
 <script setup>
-import { useForm } from '@inertiajs/vue3';
+import { useForm, router } from '@inertiajs/vue3';
+import NProgress from 'nprogress';
 import { computed } from 'vue';
 import Box from '@/Components/UI/Box.vue';
 
 const props = defineProps({
     listing: Object
+})
+
+router.on('progress', (event) => {
+    if (event.detail.progress.percentage) {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.9)
+    }
 })
 
 const form = useForm({
