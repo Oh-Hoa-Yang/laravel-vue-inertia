08f763a08d9fe2279bbd0f42a73580549c442f50
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-11 18:02:26 +0800

    feat: add P128 - Displaying the Uploaded Images

--- app/Models/ListingImage.php
+++ app/Models/ListingImage.php
@@ -12,11 +12,18 @@ class ListingImage extends Model
     use HasFactory;
 
     protected $fillable = ['filename'];
+    protected $appends = ['src'];
 
     public function listing(): BelongsTo
     {
         return $this->belongsTo(Listing::class);
         // Notice how I haven't specified the second argument to this belongs to method, which is the column name to use for the foreign key, as Laravel has a mechanism to automatically figure out this column name. 
         // In this example, if you want specify a custom column name, Laravel will just use the relation name 'listing():BelongsTo' underscore id, which will become 'listing_id'
+    }
+
+    // getRealSrcAttribute -> real_src
+    public function getSrcAttribute()
+    {
+        return asset("storage/{$this->filename}");
     }
 }
