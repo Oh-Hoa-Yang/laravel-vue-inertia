6e20d32c7b7f5f368ca73bea65929064dd73764a
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-21 23:58:05 +0800

    feat: add P145 - Simpler Way To Identify Sold Listings (Refactoring the Query Scope)

--- app/Http/Controllers/RealtorListingAcceptOfferController.php
+++ app/Http/Controllers/RealtorListingAcceptOfferController.php
@@ -13,6 +13,9 @@ class RealtorListingAcceptOfferController extends Controller
         // Accept selected offer
         $offer->update(['accepted_at' => now()]); //Pass the attribute you want to modify inside the array, just make sure that attributes you would like to modify are inside this fillable array (in Model) // => now() set it to now, which would just mean the current date, and this would immediately save the changes.
 
+        $offer->listing->sold_at = now();
+        $offer->listing->save();
+
         // Reject all other offers 
         $offer->listing->offers()->except($offer)
             ->update(['rejected_at' => now()]);
