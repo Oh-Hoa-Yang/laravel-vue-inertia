6751534d9482548d3644edcc3090af1ba1942183
Oh Hoa Yang <ohhaoyang1@gmail.com>
2026-06-25 00:17:59 +0800

    feat: add P147 - UI Improvements

--- resources/js/pages/Realtor/Show/Components/Offer.vue
+++ resources/js/pages/Realtor/Show/Components/Offer.vue
@@ -23,7 +23,7 @@
                 </div>
             </div>
             <div>
-                <Link v-if="notSold"
+                <Link v-if="!isSold"
                     :href="route('realtor.offer.accept', {offer: offer.id})"
                     class="btn-outline text-xs font-medium"
                     method="PUT"
@@ -44,6 +44,7 @@ import Box from '@/Components/UI/Box.vue';
 const props = defineProps({
     offer: Object,
     listingPrice: Number,
+    isSold: Boolean,
 })
 
 const difference = computed(
@@ -52,9 +53,5 @@ const difference = computed(
 
 const madeOn = computed(
     () => new Date(props.offer.created_at).toDateString(),
-)
-
-const notSold = computed(
-    () => !props.offer.accepted_at && !props.offer.rejected_at
 )
 </script>
